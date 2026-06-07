85a0fe61b3f6a868b4ad10bb5305a8e377fe4279
Giovanne da Silva Batista <giobatista14@gmail.com>
2026-06-07 13:41:34 -0400

    - Correção de Mensagens

--- src/1 - Frontend/BlueCleanProxyApi/Resources/StringResources.cs
+++ src/1 - Frontend/BlueCleanProxyApi/Resources/StringResources.cs
@@ -3,7 +3,7 @@ namespace BlueCleanProxyApi.Resources;
 public class StringResources
 {
   public const string NenhumRegistroEncontrado = "Nenhum Registro Encontrado.";
-  public const string ClienteErroComunicacaoBackend = "Erro ao comunicar com a API de backend.";
-  public const string LoginErroComunicacaoBackend = "Erro ao comunicar com a API de backend.";
-  public const string CepErroComunicacaoBackend = "Erro ao comunicar com a API de backend.";
+  public const string ClienteErroComunicacaoBackend = "Erro ao comunicar com a API.";
+  public const string LoginErroComunicacaoBackend = "Erro ao comunicar com a API.";
+  public const string CepErroComunicacaoBackend = "Erro ao comunicar com a API.";
 }
